e2867b0b4b87eb9e0b3358fb7fdc09c68b14243b
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-21 12:25:49 +0200

    fix some errors in signIn funtion

--- lib/auth-context.tsx
+++ lib/auth-context.tsx
@@ -1,16 +1,36 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 import { ID, Models } from "react-native-appwrite";
 import { account } from "./appwrite";
 
 type AuthContextType = {
-    // user : Models.User<Models.Preferences> | null;
+    user : Models.User<Models.Preferences> | null;
+    isLoadingUser : boolean
     signUp : (email: string, password: string) => Promise<string | null>;
     signIn : (email: string, password: string) => Promise<string | null>;
+    signOut : () => Promise<string | null>;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({children} : {children : React.ReactNode}) {
+
+    const [user, setUser] = useState<Models.User<Models.Preferences> | null>(null);
+    const [isLoadingUser, setIsLoadingUser] = useState<boolean>(true);
+
+    const getUser = async () => {
+        try{
+            const session = await account.get();
+            setUser(session);
+        } catch(error) {
+            setUser(null);
+        } finally {
+            setIsLoadingUser(false);
+        }
+    }
+
+    useEffect(()=> {
+        getUser();
+    }, [])
 
     const signUp = async (email: string, password: string) => {
         try{
@@ -29,6 +49,8 @@ export function AuthProvider({children} : {children : React.ReactNode}) {
     const signIn = async (email: string, password: string) => {
         try{
             await account.createEmailPasswordSession(email, password);
+            const session = await account.get();
+            setUser(session);
             return null;
 
         } catch(error) {
@@ -39,9 +61,22 @@ export function AuthProvider({children} : {children : React.ReactNode}) {
         }
     }
 
+    const signOut = async () => {
+        try {
+            await account.deleteSession("current");
+            setUser(null);
+            return null;
+        } catch(error) {
+            if(error instanceof Error) {
+                return error.message;
+            }
+            return "An error occurred during Sign Out";
+        }
+    };
+
     return (
         <AuthContext.Provider
-           value={{signIn, signUp}}
+           value={{user, isLoadingUser, signIn, signUp, signOut}}
         >
             {children}
         </AuthContext.Provider>
