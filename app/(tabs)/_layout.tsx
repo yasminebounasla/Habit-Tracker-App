e6eb7e7815c3bd34fa716c6f0eed7bd65240d102
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-29 08:16:57 +0200

    correct tab colors and add SafeArea/Paper providers

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,16 +4,16 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 export default function TabsLayout() {
   return (
     <Tabs screenOptions={{ 
-      headerStyle : { backgroundColor:"f5f5f5"},
+      headerStyle : { backgroundColor:"#f5f5f5"},
       headerShadowVisible: false, 
       tabBarStyle: {
-        backgroundColor:"f5f5f5",
+        backgroundColor:"#f5f5f5",
         borderTopWidth: 0,
         elevation: 0,
         shadowOpacity: 0
       },
-      tabBarActiveTintColor: "6200ee",
-      tabBarInactiveTintColor: "666666"
+      tabBarActiveTintColor: "#6200ee",
+      tabBarInactiveTintColor: "#666666"
     }}>
 
       <Tabs.Screen  
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
 import { Stack, useRouter, useSegments } from "expo-router";
 import { useEffect } from "react";
 import { AuthProvider, useAuth } from "@/lib/auth-context";
+import { PaperProvider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 function RouteGuard({children} :{ children : React.ReactNode }) {
   const router = useRouter();
@@ -27,12 +29,16 @@ function RouteGuard({children} :{ children : React.ReactNode }) {
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <RouteGuard>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-          <Stack.Screen name="auth" options={{title : "Auth"}}/>
-        </Stack>
-      </RouteGuard>
+      <PaperProvider>
+        <SafeAreaProvider>
+          <RouteGuard>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+              <Stack.Screen name="auth" options={{title : "Auth"}}/>
+            </Stack>
+          </RouteGuard>
+        </SafeAreaProvider>
+      </PaperProvider>
     </AuthProvider>
   );
 }
