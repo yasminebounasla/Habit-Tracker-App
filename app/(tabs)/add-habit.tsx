0fc20c636c3d28a97a8d80c232644e83cf1e38b7
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-29 10:13:50 +0200

    add addHabit states and implement handleSubmit

--- app/(tabs)/add-habit.tsx
+++ app/(tabs)/add-habit.tsx
@@ -1,16 +1,62 @@
 import { Label } from "@react-navigation/elements";
 import { View, Text, StyleSheet } from "react-native";
+import { DATABASE_ID, databases, HABITS_COLLECTION_ID } from "@/lib/appwrite";
 import { SegmentedButtons, TextInput, Button } from "react-native-paper";
+import { useState } from "react";
+import { useAuth } from "@/lib/auth-context";
+import { ID } from "react-native-appwrite";
+import { useRouter } from "expo-router";
 
 const FREQUENCIES = ["daily", "weekly", "monthly"];
+type Frequency = (typeof FREQUENCIES)[number];
+const {user} = useAuth();
+const router = useRouter()
+
+const [title, setTitle] = useState<String>("");
+const [description, setDescription] = useState<String>("");
+const [frequency, setFrequency] = useState<Frequency>("daily");
+
+
+const handleSubmit = async () => {
+    if(!user) return;
+
+    await databases.createDocument(
+        DATABASE_ID, 
+        HABITS_COLLECTION_ID, 
+        ID.unique(),
+        {
+            user_id : user.$id,
+            title,
+            description,
+            frequency,
+            streak_count: 0,
+            last_completed: new Date().toISOString(),
+            $createdAt: new Date().toISOString()   
+        }
+    );
+
+    router.back()
+}
 
 export default function AddHabitScreen() {
     return (
         <View style={styles.container}>
-            <TextInput label="Title" mode="outlined" style={styles.input}/>
-            <TextInput label="Description" mode="outlined" style={styles.input}/>
+            <TextInput 
+               label="Title" 
+               mode="outlined" 
+               style={styles.input}
+               onChangeText={setTitle}
+            />
+            <TextInput 
+               label="Description" 
+               mode="outlined" 
+               style={styles.input}
+               onChangeText={setDescription}
+            />
             <View style={styles.frequencyContainer}>
                 <SegmentedButtons 
+                    value={frequency}
+                    onValueChange={(value) => setFrequency(value as Frequency)}
                     buttons={FREQUENCIES.map((freq) => ({
                         value: freq,
                         label: freq.charAt(0).toUpperCase() + freq.slice(1),
@@ -19,6 +65,8 @@ export default function AddHabitScreen() {
             </View>  
             <Button 
                mode="contained"
+               disabled={!title || !description}
+               onPress={handleSubmit}
             >
                 Add Habit
             </Button>
