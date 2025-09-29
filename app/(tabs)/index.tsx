da2f583943392f2be143f1e5ed7e40ff60eafa9e
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-29 12:26:30 +0200

    add fetch Habits funtion

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,30 @@
 import { Text, View , StyleSheet} from "react-native";
 import { Button } from "react-native-paper";
 import { useAuth } from "@/lib/auth-context";
+import { DATABASE_ID, databases, HABITS_COLLECTION_ID } from "@/lib/appwrite";
+import { Query } from "react-native-appwrite";
+import { useState } from "react";
+import { Habit } from "@/types/database.type";
 
 export default function Index() {
-  const {signOut} = useAuth();
+  const {signOut, user} = useAuth();
+
+  const [habits, setHabits] = useState<Habit[]>();
+
+  const fetchHabits = async () => {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        HABITS_COLLECTION_ID,
+        [Query.equal("user_id", user?.$id ?? "")]
+      );
+      
+      setHabits(response.documents as unknown as Habit[]);
+
+    } catch(err) {
+      console.error(err);
+    }
+  }
   return (
     <View
       style={styles.view}
