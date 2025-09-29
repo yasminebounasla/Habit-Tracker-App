1bf076c69a06b53686d4bb754778736808ffd67f
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-29 08:39:44 +0200

    add Appwrite database and habits collection env vars

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,8 +1,12 @@
-import { Account, Client } from "react-native-appwrite";
+import { Account, Client, Databases } from "react-native-appwrite";
 
 export const client = new Client()
    .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
    .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!)
 
-   export const account = new Account(client)
+export const account = new Account(client);
+export const databases = new Databases(client);
+
+export const DATABASE_ID = process.env.EXPO_PUBLIC_DB_ID!
+export const HABITS_COLLECTION_ID = process.env.EXPO_PUBLIC_HABITS_COLLECTION_ID!;
