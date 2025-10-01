87fc1e75e3f7e0585d9b12eb3adc94e66ba14134
yasmine bounasla <yasminebounasla@gmail.com>
2025-10-01 14:25:39 +0200

    make the screen scrollable

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,11 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import { Button, Surface, Text } from "react-native-paper";
 import { useAuth } from "@/lib/auth-context";
-import { DATABASE_ID, databases, HABITS_COLLECTION_ID } from "@/lib/appwrite";
+import { client, DATABASE_ID, databases, HABITS_COLLECTION_ID, RealTimeResponse } from "@/lib/appwrite";
 import { Query } from "react-native-appwrite";
 import { useState, useEffect } from "react";
 import { Habit } from "@/types/database.type";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { useFocusEffect } from "@react-navigation/native";
 import { useCallback } from "react";
 
 export default function Index() {
@@ -26,66 +25,70 @@ export default function Index() {
       console.error(err);
     }
   }
-
   
-  useFocusEffect(
+  useEffect(
     useCallback(() => {
       fetchHabits();
-    }, [user?.$id])
+    }, [user])
   );
 
   return (
-    <View>
-      <View style={styles.header}>
-        <Text variant="headlineSmall" style={styles.header}>
-          Today's Habits
-        </Text>
-        <Button mode="text" onPress={signOut} icon={"logout"}>
-          Sign Out
-        </Button>
-      </View>
-      {habits.length === 0 ? (
-        <View style={styles.emptyState}>
-          <Text style={styles.emptyStateText}>No Habits Yet. Add Your First Habit!</Text>
+    <ScrollView style={styles.scrollContainer}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text variant="headlineSmall" style={styles.header}>
+            Today's Habits
+          </Text>
+          <Button mode="text" onPress={signOut} icon={"logout"}>
+            Sign Out
+          </Button>
         </View>
-      ) : (
-        habits.map((habit, key) => (
-          <Surface style={styles.card} >
-            <View key={key} style={styles.cardContent}>
-              <Text style={styles.cardTitle}>{habit.title}</Text>
-              <Text style={styles.cardDescription}>{habit.description}</Text>
-              <View style={styles.cardFooter}>
-                <View style={styles.streakBadge}>
-                  <MaterialCommunityIcons
-                    name="fire"
-                    size={18}
-                    color="#ff9800"
-                  />
-                  <Text style={styles.streakText}>
-                    {habit.streak_count} day streak
-                  </Text> 
-                </View>
-                <View style={styles.frequencyBadge}>
-                  <Text style={styles.frequencyText}>
-                    {" "}
-                    {habit.frequency.charAt(0).toUpperCase() +
-                      habit.frequency.slice(1)}
-                  </Text>
+        {habits.length === 0 ? (
+          <View style={styles.emptyState}>
+            <Text style={styles.emptyStateText}>No Habits Yet. Add Your First Habit!</Text>
+          </View>
+        ) : (
+          habits.map((habit, key) => (
+            <Surface style={styles.card} key={key}>
+              <View style={styles.cardContent}>
+                <Text style={styles.cardTitle}>{habit.title}</Text>
+                <Text style={styles.cardDescription}>{habit.description}</Text>
+                <View style={styles.cardFooter}>
+                  <View style={styles.streakBadge}>
+                    <MaterialCommunityIcons
+                      name="fire"
+                      size={18}
+                      color="#ff9800"
+                    />
+                    <Text style={styles.streakText}>
+                      {habit.streak_count} day streak
+                    </Text> 
+                  </View>
+                  <View style={styles.frequencyBadge}>
+                    <Text style={styles.frequencyText}>
+                      {" "}
+                      {habit.frequency.charAt(0).toUpperCase() +
+                        habit.frequency.slice(1)}
+                    </Text>
+                  </View>
                 </View>
               </View>
-            </View>
-          </Surface>
-        ))
-      )}
-    </View>
+            </Surface>
+          ))
+        )}
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: "#f5f5f5",
   },
   header: {
     flexDirection: "row",
@@ -106,8 +109,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.08,
     shadowRadius: 8,
     elevation: 4,
-    marginRight: 15,
-    marginLeft : 15,
   },
 
   cardCompleted: {
