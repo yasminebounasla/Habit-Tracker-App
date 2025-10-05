82482cb076fc34624493acf796c0e6ad0cc7c398
yasmine bounasla <yasminebounasla@gmail.com>
2025-10-05 18:05:51 +0200

    Creating the Streaks Screen

--- app/(tabs)/streaks.tsx
+++ app/(tabs)/streaks.tsx
@@ -1,7 +1,96 @@
+import { client, COMPLETIONS_COLLECTION_ID, DATABASE_ID, databases, HABITS_COLLECTION_ID, RealtimeResponse } from "@/lib/appwrite";
+import { useAuth } from "@/lib/auth-context";
+import { Habit, HabitCompletion } from "@/types/database.type";
+import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
+import { Query } from "react-native-appwrite";
 
 
 export default function StreaksScreen() {
+
+   const [habits, setHabits] = useState<Habit[]>([]);
+   const [completedHabits, setCompletedHabits] = useState<HabitCompletion[]>([]);
+   const { user } = useAuth();
+
+    useEffect(() => {
+    if (user) {
+      const habitsChannel = `databases.${DATABASE_ID}.collections.${HABITS_COLLECTION_ID}.documents`;
+      const habitsSubscription = client.subscribe(
+        habitsChannel,
+        (response: RealtimeResponse) => {
+          if (
+            response.events.includes(
+              "databases.*.collections.*.documents.*.create"
+            )
+          ) {
+            fetchHabits();
+          } else if (
+            response.events.includes(
+              "databases.*.collections.*.documents.*.update"
+            )
+          ) {
+            fetchHabits();
+          } else if (
+            response.events.includes(
+              "databases.*.collections.*.documents.*.delete"
+            )
+          ) {
+            fetchHabits();
+          }
+        }
+      );
+
+      const completionsChannel = `databases.${DATABASE_ID}.collections.${COMPLETIONS_COLLECTION_ID}.documents`;
+      const completionsSubscription = client.subscribe(
+        completionsChannel,
+        (response: RealtimeResponse) => {
+          if (
+            response.events.includes(
+              "databases.*.collections.*.documents.*.create"
+            )
+          ) {
+            fetchCompletions();
+          }
+        }
+      );
+
+      fetchHabits();
+      fetchCompletions();
+
+      return () => {
+        habitsSubscription();
+        completionsSubscription();
+      };
+    }
+  }, [user]);
+
+   const fetchHabits = async () => {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        HABITS_COLLECTION_ID,
+        [Query.equal("user_id", user?.$id ?? "")]
+      );
+      setHabits(response.documents as unknown as Habit[]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchCompletions = async () => {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        COMPLETIONS_COLLECTION_ID,
+        [Query.equal("user_id", user?.$id ?? "")]
+      );
+      const completions = response.documents as unknown as HabitCompletion[];
+      setCompletedHabits(completions);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
     return (
         <View>
             <Text>Hello this is the Login page</Text>
